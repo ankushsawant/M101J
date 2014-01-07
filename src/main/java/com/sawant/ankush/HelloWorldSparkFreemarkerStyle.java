e1e257460f1d565c01924334aa55572b8392765b
Ankush Sawant <ankushsawant@yahoo.com>
2014-01-07 15:32:35 -0500

    Add HelloWorld SparkJava Freemarker Style

---
+++ src/main/java/com/sawant/ankush/HelloWorldSparkFreemarkerStyle.java
@@ -0,0 +1,48 @@
+package com.sawant.ankush;
+
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+
+public class HelloWorldSparkFreemarkerStyle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		final Configuration configuration = new Configuration();
+		configuration.setClassForTemplateLoading(HelloWorldFreemarkerStyle.class, "/");
+		
+		Spark.get(new Route("/") {
+			
+			@Override
+			public Object handle(Request arg0, Response arg1) {
+				// TODO Auto-generated method stub
+				
+				StringWriter writer = new StringWriter();
+
+				try {
+					Template helloTemplate = configuration.getTemplate("hello.ftl");
+					
+					Map<String, Object> helloMap = new HashMap<String, Object>();
+					helloMap.put("name", "Freemarker");
+					
+					helloTemplate.process(helloMap, writer);
+										
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					halt(500);
+					e.printStackTrace();
+				}
+				return writer;
+			}
+		});
+	}
+
+}
